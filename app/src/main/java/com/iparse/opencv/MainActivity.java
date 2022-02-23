cdbd563de65b9ca8d80c02f9481106e90e082a9d
Ajay Kumar <ajay.kumar@kiwitech.com>
2022-02-24 00:59:36 +0530

    static lib integration

--- app/src/main/java/com/iparse/opencv/MainActivity.java
+++ app/src/main/java/com/iparse/opencv/MainActivity.java
@@ -3,13 +3,38 @@ package com.iparse.opencv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+
+/*    private static boolean mLibrariesLoaded = false;
+    private static boolean needsPermissionCheck = true;
+
+    static {
+        System.loadLibrary("libopencv_core");
+        if (OpenCVLoader.initDebug()) {
+            mLibrariesLoaded = true;
+            Log.e("TAG", "------------------------OpenCV loaded");
+        } else {
+            Log.e("TAG", "------------------------OpenCV load failed");
+        }
+
+        needsPermissionCheck = true;
+    }*/
+
+    public native String stringFromJNI(String str);
+
+    static {
+        System.loadLibrary("opencv-static-libs");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ((TextView)findViewById(R.id.textView)).setText(stringFromJNI("Hey this is from JNI"));
     }
 }
